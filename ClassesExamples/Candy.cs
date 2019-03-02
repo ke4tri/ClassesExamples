4509e38a6e4c3fccd910f8a0368f9837c0528f0b
Wayne Collier <ke4tri@gmail.com>
2019-03-02 13:33:36 -0600

    examples

---
+++ ClassesExamples/Candy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExamples
+{
+    class Candy
+    {
+        //Properties
+        public string Flavor { get; private set; }
+        public int Rating { get; private set; }
+        public string Review { get; private set; };
+        public string Name { get; private set; }
+        public CandyType Type { get; private set; }
+
+        //Fields
+        const int MinRatingValue = 1;  //Feild
+        const int MaxRatingValue = 10;  //Feild
+
+        //Constructor
+        public Candy(string flavor,string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type; 
+        }
+
+        //Methods
+        public void SetRating(int userRating)
+        {
+            if (userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue}. You moron.");
+            }
+        }
+
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+    }
+
+    enum CandyType // Are just numbers under the hood
+    {
+        Chocolate,
+        HardCandy,
+        Caramel,
+        Sour,
+        Stretchy,
+        Nut
+    }
+}
--- ClassesExamples/Program.cs
+++ ClassesExamples/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace ClassesExamples
+namespace ClassesExamples //NameSpace
 {
     class Program
     {
-        static void Main(string[] args)
+
+        //private string _myData = "this is just for me to use"; //Field
+
+        //public int Number { get; set; } //Property
+
+        //public void DoTheTHing() Propertices, caps
+        //{
+        //    Number = 1;
+        //    var x = Number;
+        //}
+
+        private static void Main(string[] args) //Method. by nature is private can leave out
         {
+            //var taffy = new Candy();
+            //taffy.Flavor = "Orange";
+            //taffy.Name = "Orange Saltwater Taffy";
+            //taffy.Rating = 5;
+            //taffy.Type = CandyType.Stretchy;
+
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+
+            taffy.SetRating(10);
+            taffy.SetRating(1);
+                
             Console.WriteLine("Hello World!");
         }
     }
 }
+
+
+
+
